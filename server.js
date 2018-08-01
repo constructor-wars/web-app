de7715b0ab1bcd5385317e0b05ebde9d143eed08
James McAllister <jrfmcallister@gmail.com>
2018-08-01 13:51:10 +0100

    ...

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use("/dist", express.static(path.join(__dirname + "/dist")));
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 const port = process.env.PORT || 8080;
