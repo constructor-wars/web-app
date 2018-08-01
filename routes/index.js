f5b8becdb4d38649c621a60860a402ab6908c0fb
Rafal <r.porzucek@gmail.com>
2018-08-01 14:17:04 +0100

    Merge done now implementing

---
+++ routes/index.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const express = require("express");
+const passport = require("passport");
+
+const router = express.Router();
+
+/*get home page*/
+
+router.get("/", function(req, res, next) {
+  // passport.authenticate("github", function(err, user, info) {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   if (!user) {
+  //     return res.redirect("/login");
+  //   } else
+  //     return req.logIn(user, function(err) {
+  //       if (err) {
+  //         return next(err);
+  //       }
+  //       return res.redirect("/profile/" + user.username);
+  //     });
+  // });
+  res.render("profile", { title: "Constructor Wars", user: req.user });
+});
+
+router.get("/login", function(req, res, next) {
+  res.render("login", { title: "Login", user: req.user });
+});
+
+router.get("/profile", function(req, res, next) {
+  res.render("profile", { title: "Profile" });
+});
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/login");
+});
+
+router.get("/auth/github", passport.authenticate("github"));
+
+router.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+
+module.exports = router;
