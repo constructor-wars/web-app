a5141b39c79ac56f4a0841727cebda25e0d5c6a6
James McAllister <jrfmcallister@gmail.com>
2018-08-02 16:28:08 +0100

    getting data from githug and passing it to redux and then on to the profile compnent

--- src/Containers/ProfileContainer.js
+++ src/Containers/ProfileContainer.js
@@ -1,14 +1,22 @@
-import { connect } from 'react-redux';
-import Profile from '../components/Profile/Profile';
-import profilepic from '../images/potatoe.jpg';
+import { connect } from "react-redux";
+import Profile from "../components/Profile/Profile";
 
-const mapReduxStateToProps = reduxState => ({
-  username: 'It is ME, the user!',
-  cohort: '#2',
-  lastLogin: '01-01-2010 12:30',
-  profilepic: profilepic,
-  profileDesciption: '**Markdown is awesome!**'
-});
+const mapReduxStateToProps = reduxState => {
+  const {
+    id,
+    displayName,
+    profileUrl,
+    username,
+    photos
+  } = reduxState.GITHUB_DATA;
+  return {
+    id,
+    username,
+    displayName,
+    profileUrl,
+    profilepic: photos[0].value
+  };
+};
 
 const mapDispatchToProps = dispatch => ({});
 
