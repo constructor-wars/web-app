10f7da103961e3507d39bee3e8d404beff2fae1e
Julius <juliusvaiciunas@gmail.com>
2018-08-02 16:42:37 +0100

    before james

---
+++ src/components/NavBarProfile/NavBarProfile.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./navbarprofile.css";
+
+export default function NavBarProfile() {
+  return (
+    <div className="navbarprofile_container">
+      <img src="" />
+    </div>
+  );
+}
