97ac02d9d363330fe4abf1db694b9fea09c5430d
James McAllister <jrfmcallister@gmail.com>
2018-08-03 15:26:51 +0100

    unknown

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import './StyleEditor.css';
-import Instructions from '../Instructions/Instructions';
-
+import React from "react";
+import "./StyleEditor.css";
+import Instructions from "../Instructions/Instructions";
+import MonacoEditor from "./MonacoEditor";
+import DisplayConsole from "./DisplayConsole";
+import EvalWindow from "./EvalWindow";
 export default class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      codeToEval: ``
+    };
+  }
+  tranferCode = x => {
+    console.log(x);
+  };
   render() {
     return (
       <div>
@@ -15,12 +26,13 @@ export default class Editor extends React.Component {
           </div>
           <div className="editor__wrap__edit-window editor__sections">
             3 edit-window
+            <MonacoEditor tranferCode={this.tranferCode} />
           </div>
           <div className="editor__wrap__display-window editor__sections">
-            4 display-window
+            <EvalWindow codeToEval={this.state.codeToEval} />
           </div>
           <div className="editor__wrap__test-window editor__sections">
-            5 test-window
+            <DisplayConsole data={this.state.codeToEval} />
           </div>
         </div>
       </div>
