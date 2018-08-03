1fd33a4e964fd5890dbf65c6bc123b31bf4a8b17
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2018-08-03 14:09:06 +0100

    search basic functionality working

--- src/components/Header/Searchbar.js
+++ src/components/Header/Searchbar.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React from "react";
 
-export default function Searchbar() {
-  return (
-    <div>
-      <input className="searchbar" />
-    </div>
-  );
+class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userSearch: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchResults = this.fetchResults.bind(this);
+  }
+  handleChange(event) {
+    event.preventDefault();
+    //console.log("hello", event.target.value);
+    this.setState({
+      userSearch: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    event.preventDefault();
+    this.fetchResults(this.state.userSearch);
+  }
+
+  fetchResults() {
+    const makeHTML = text => {
+      const target = document.querySelector("#target");
+      const div = document.createElement("div");
+      div.innerHTML = text;
+      const items = div.querySelectorAll(".result-list-item h4 a");
+      target.innerHTML = [...items]
+        .map(
+          item =>
+            `<li><a href=${item.href} target="_blank">${
+              item.textContent
+            }  </a></li>`
+        )
+        .join("");
+    };
+
+    const fetchResults = q => {
+      const url = `https://developer.mozilla.org/en-US/search?q=${q}`;
+      fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+        .then(res => res.text())
+        .then(text => makeHTML(text))
+        .catch(err => console.log(err));
+    };
+
+    fetchResults();
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          className="searchbar"
+          placeholder="Feeling stuck..? get some help"
+          onChange={this.handleChange}
+        />
+        <button type="Submit" onClick={this.handleSubmit}>
+          Submit
+        </button>
+        <ul id="target" />
+      </div>
+    );
+  }
 }
+
+export default Searchbar;
