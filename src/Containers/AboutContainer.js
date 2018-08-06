9ec40cde323bccc214e306d2a48bc7039765fde9
Julius <juliusvaiciunas@gmail.com>
2018-08-06 11:19:47 +0100

    Removed progress route

--- src/Containers/AboutContainer.js
+++ src/Containers/AboutContainer.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import Home from "../components/Home/Home";
+import About from "../components/About/About";
 
 const mapReduxStateToProps = reduxState => ({});
 
@@ -8,4 +8,4 @@ const mapDispatchToProps = dispatch => ({});
 export default connect(
   mapReduxStateToProps,
   mapDispatchToProps
-)(Home);
+)(About);
