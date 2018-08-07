c52a2405f3787f1200e220897dd8d62f548d005e
Rafal <r.porzucek@gmail.com>
2018-08-07 09:08:17 +0100

    api reducers and actions

---
+++ src/reducers/apiReducers.js
@@ -0,0 +1,53 @@
+export function userByUsername(state = "", action) {
+  switch (action.type) {
+    case "USER_BY_USERNAME":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export function questionById(state = "", action) {
+  switch (action.type) {
+    case "QUESTION_BY_ID":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export function allQuestions(state = {}, action) {
+  switch (action.type) {
+    case "ALL_QUESTIONS":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export function sendQuestionToDatabase(state = {}, action) {
+  switch (action.type) {
+    case "SEND_QUESTION_TO_DATABASE":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export function userData(state = {}, action) {
+  switch (action.type) {
+    case "USER_DATA":
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export function userProgress(state = 0, action) {
+  switch (action.type) {
+    case "USER_PROGRESS":
+      return action.payload;
+    default:
+      return state;
+  }
+}
