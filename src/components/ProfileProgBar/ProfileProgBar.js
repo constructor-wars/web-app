8af0a3a698b1cb5e0b52f467404561d0d7da52b4
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2018-08-07 10:32:16 +0100

    looking sweet!!

--- src/components/ProfileProgBar/ProfileProgBar.js
+++ src/components/ProfileProgBar/ProfileProgBar.js
@@ -1,30 +1,21 @@
 import React from "react";
-import Bar from "./Bar";
 
 class ProfileProgBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      percentage: 55
-    };
-  }
-
   render() {
     return (
       <div className="profile__progress__bar">
-        <p>our fucking bar</p>
-        <Bar percentage={this.state.percentage} />
+        <h2>You current progress</h2>
+        <div className="progress__bar">
+          <div
+            className="progress__bar__filler"
+            style={{
+              width: `${(this.props.current / this.props.total) * 100}%`
+            }}
+          />
+        </div>
       </div>
     );
   }
 }
 
 export default ProfileProgBar;
-
-{
-  /* <span className="total__progress">
-          {this.props.total}
-          <p className="current__progress"> {this.props.current} </p>
-        </span> */
-}
