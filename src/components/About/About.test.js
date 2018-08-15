c16acc42ad39a5a43bf24d719517d61715d24e5a
Julius <juliusvaiciunas@gmail.com>
2018-08-15 09:55:10 +0100

    About snapshot

---
+++ src/components/About/About.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./About";
+import renderer from "react-test-renderer";
+
+describe("About", () => {
+  it("matches the snapshot", () => {
+    const tree = renderer.create(<About />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+<div
+  className="about__wrapper"
+>
+  <h1>
+    From Potatoe to Camel
+  </h1>
+  Lorem ipsum dolor sit amet, commodo fringilla, eu sagittis cras, ultrices id ac autem pulvinar a posuere, facilisi massa cras non quam non, lectus morbi lacinia eget. Ligula sit fringilla felis scelerisque vestibulum, integer sed fermentum pede dolor a, eros porta, euismod lectus velit duis, per condimentum aenean erat consequat dictumst. Nec mus leo hendrerit sit lacus, integer urna dignissim ut imperdiet, nunc tempus lorem wisi vestibulum vivamus non, convallis feugiat bibendum. Odio vivamus commodo, mollis nunc, ut consequat quis vitae luctus et, interdum iaculis amet, pellentesque orci vestibulum ac dui id. Lacus tincidunt ut quia ultricies. Integer vestibulum diam amet nec interdum lobortis, metus non magnis posuere. In erat, nibh vulputate rhoncus aenean lobortis excepturi, id luctus vitae per, vehicula neque a vitae donec aliquam, cursus eget eleifend morbi. Ut donec ante nascetur tempora lobortis eleifend, dignissim dui morbi elementum cum amet vel. Per morbi velit, vel tempor maecenas proin sit morbi. Facilisis et non mollis mi pede metus, vitae magna vehicula nulla, dolor nunc dictum maecenas nibh lectus. Ultricies integer non, ullamcorper ut non neque sem fames erat. Fusce dui aenean pellentesque venenatis, tellus integer magna vestibulum ipsum suspendisse mauris, in pede purus, accusamus tincidunt tristique dictum ipsum commodo, nec molestie dapibus consequat wisi. Nullam tristique vel, non eveniet ultrices iaculis sed eligendi. Pede et vehicula et lobortis mauris, ut neque. Duis laborum, convallis ligula arcu sit, interdum erat leo aenean augue ut etiam, lorem neque ante maecenas dolor, sollicitudin non. Porttitor tincidunt orci eu urna, tincidunt ac eros luctus habitasse in nulla, ridiculus morbi, lacus mi dictum tempor tincidunt placerat ac, cras posuere blandit.
+</div>
+`);
+  });
+});
